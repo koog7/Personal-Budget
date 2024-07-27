d1ee0ae1ee86dec2134c7218d4a2aaa131ef5f96
koog7 <k.ognev709@mail.com>
2024-07-27 13:16:31 +0700

    Создана страница с формой со стилями

--- src/containers/AddBlock.tsx
+++ src/containers/AddBlock.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-
 const AddBlock = () => {
     return (
-        <div>
-            add form
+        <div className="form-wrapper">
+            <form className="add-form">
+                <h2 className="title">Add Form</h2>
+                <div className="form-group">
+                    <label htmlFor="type" className="label">Type</label>
+                    <select id="type" name="type" className="form-input">
+                        <option value="income">Income</option>
+                        <option value="expense">Expense</option>
+                    </select>
+                </div>
+                <div className="form-group" style={{width:'383px'}}>
+                    <label htmlFor="name" className="label">Name</label>
+                    <input type="text" id="name" name="name" className="form-input"/>
+                </div>
+                <button type="submit" className="form-button">Submit</button>
+            </form>
         </div>
     );
 };
